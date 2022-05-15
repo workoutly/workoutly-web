37fd7fa66464ae0934eb76873782c0f308fd245e
moruno21 <amoruno21@gmail.com>
2022-05-15 22:18:27 +0200

    a tomar por culo

--- components/RoutineCard.tsx
+++ components/RoutineCard.tsx
@@ -1,32 +1,72 @@
 import Card from '@mui/material/Card';
-import { CardActions } from '@mui/material';
+import { CardActions, useStepperContext } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import { TextField } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Autocomplete from '@mui/material/Autocomplete';
+import styles from '../styles/Home.module.css';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Button } from '@mui/material';
 import { useRouter } from 'next/router';
+import Grid from '@mui/material/Grid';
+import { useEffect, useState } from 'react';
 
-const RoutineCard = ({ routine }: { routine: number }) => {
+const RoutineCard = ({ routine }) => {
   const router = useRouter();
 
   return (
-    <Grid item xs={2} sm={4} md={4}>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Título
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Creador
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button onClick={() => router.push(`routines/${routine}`)} variant="contained">
-            Ver rutina
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
+    <>
+      <Grid item xs={2} sm={4} md={4}>
+        <Card sx={{ display: 'inline-block' }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {routine._name}
+            </Typography>
+          </CardContent>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Nombre</TableCell>
+                  <TableCell align="left">Descripción</TableCell>
+                  <TableCell align="left">Músculos</TableCell>
+                  <TableCell align="left">Ajustes</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {routine?._workouts.map((workout) => (
+                  <TableRow
+                    key={workout.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {workout.name}
+                    </TableCell>
+                    <TableCell align="left">{workout.description}</TableCell>
+                    <TableCell align="left">
+                      {workout.muscles.map((muscle) => `${muscle.name} `)}
+                    </TableCell>
+                    <TableCell align="left">
+                      {workout.settings.map((setting) => `${setting.value}${setting.name} `)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Card>
+      </Grid>
+    </>
   );
 };
 
--- components/RoutinesList.tsx
+++ components/RoutinesList.tsx
@@ -1,17 +1,110 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import RoutineCard from './RoutineCard';
+import { Button } from '@mui/material';
+import styles from '../styles/Home.module.css';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import { TextField } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useEffect, useState } from 'react';
+import Autocomplete from '@mui/material/Autocomplete';
 
 const RoutinesList = () => {
-  const routines: number[] = [1, 2, 3, 4, 5];
+  const [newRoutine, setNewRoutine] = useState();
+  const [routines, setRoutines] = useState();
+
+  useEffect(() => {
+    const getRoutines = async () => {
+      const res = await fetch(`http://localhost:3333/routines`, {
+        method: 'GET',
+      });
+      const data = await res.json();
+      setRoutines(data);
+    };
+    getRoutines();
+  }, []);
+
+  const workouts = [
+    { label: 'Press de banca', muscle: 'Pectoral' },
+    { label: 'Sentadillas', muscle: 'Pierna' },
+    { label: 'Jalón al pecho', muscle: 'Espalda' },
+    { label: 'Curl de biceps', muscle: 'Biceps' },
+    { label: 'Abdominales', muscle: 'Abdominales' },
+    { label: 'Press americano', muscle: 'Hombros' },
+    { label: 'Extensión de triceps', muscle: 'Triceps' },
+  ];
 
   return (
     <Box sx={{ flexGrow: 1, m: 8 }}>
       <Grid container spacing={{ xs: 1, sm: 2, md: 3 }} columns={{ xs: 1, sm: 8, md: 12 }}>
-        {routines?.map((routine: number) => (
-          <RoutineCard key={routine} routine={routine} />
+        {routines?.map((routine) => (
+          <RoutineCard key={routine._id} routine={routine} />
         ))}
       </Grid>
+      <Accordion sx={{ m: 4 }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography sx={{ fontWeight: 'bold' }}>Añadir rutina</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{ '& .MuiTextField-root': { m: 2, width: '25ch' } }}>
+          <Grid container justifyContent={'center'}>
+            <Grid container justifyContent={'center'} item xs={12} sm={6} md={3}>
+              <TextField id="name" label="Nombre" variant="outlined" color="primary" />
+            </Grid>
+            <Grid container justifyContent={'center'} item xs={12} sm={6} md={3}>
+              <TextField id="description" label="Descripción" variant="outlined" color="primary" />
+            </Grid>
+          </Grid>
+          <hr />
+          <Grid container justifyContent={'center'} spacing={2}>
+            <Grid container justifyContent={'center'} item xs={8} sm={8} md={6} lg={4} xl={3}>
+              <Autocomplete
+                disablePortal
+                id="workout"
+                options={workouts}
+                renderInput={(params) => <TextField {...params} label="Ejercicio" />}
+              />
+              <TextField id="settings" label="Ajustes" variant="outlined" color="primary" />
+            </Grid>
+            <Grid container justifyContent={'center'} item xs={8} sm={8} md={6} lg={4} xl={3}>
+              <Autocomplete
+                disablePortal
+                id="workout"
+                options={workouts}
+                renderInput={(params) => <TextField {...params} label="Ejercicio" />}
+              />
+              <TextField id="settings" label="Ajustes" variant="outlined" color="primary" />
+            </Grid>
+            <Grid container justifyContent={'center'} item xs={8} sm={8} md={6} lg={4} xl={3}>
+              <Autocomplete
+                disablePortal
+                id="workout"
+                options={workouts}
+                renderInput={(params) => <TextField {...params} label="Ejercicio" />}
+              />
+              <TextField id="settings" label="Ajustes" variant="outlined" color="primary" />
+            </Grid>
+            <Grid container justifyContent={'center'} item xs={8} sm={8} md={6} lg={4} xl={3}>
+              <Autocomplete
+                disablePortal
+                id="workout"
+                options={workouts}
+                renderInput={(params) => <TextField {...params} label="Ejercicio" />}
+              />
+              <TextField id="settings" label="Ajustes" variant="outlined" color="primary" />
+            </Grid>
+          </Grid>
+          <div className={styles.saveRoutineButton}>
+            <Button variant="contained">Crear rutina</Button>
+          </div>
+        </AccordionDetails>
+      </Accordion>
     </Box>
   );
 };
