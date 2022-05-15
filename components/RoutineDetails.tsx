e699e268909f8c2b76996b55df788305a469595f
moruno21 <amoruno21@gmail.com>
2022-05-15 18:35:27 +0200

    feat: add workout to routine form

--- components/RoutineDetails.tsx
+++ components/RoutineDetails.tsx
@@ -5,11 +5,19 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import styles from '../styles/Home.module.css';
 import { Button } from '@mui/material';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import { TextField } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Autocomplete from '@mui/material/Autocomplete';
+import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { flexbox } from '@mui/system';
 
 const RoutineDetails = () => {
   const router = useRouter();
@@ -26,7 +34,14 @@ const RoutineDetails = () => {
     getWorkouts();
   }, []);
 
-  console.log(workouts);
+  const existingWorkouts = [
+    { label: 'Press banca' },
+    { label: 'Sentadilla' },
+    { label: 'Press americano' },
+    { label: 'Jalón al pecho' },
+    { label: 'Abdominales' },
+    { label: 'Curl biceps' },
+  ];
 
   return (
     <>
@@ -70,9 +85,28 @@ const RoutineDetails = () => {
           </TableBody>
         </Table>
       </TableContainer>
-      <div className={styles.saveRoutineButton}>
-        <Button variant="contained">Añadir ejercicio</Button>
-      </div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography sx={{ fontWeight: 'bold' }}>Añadir ejercicio</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{ '& .MuiTextField-root': { m: 2, width: '25ch' } }}>
+          <Autocomplete
+            disablePortal
+            id="workout"
+            options={existingWorkouts}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Ejercicio" />}
+          />
+          <TextField id="settings" label="Ajustes" variant="outlined" color="primary" />
+          <div className={styles.saveRoutineButton}>
+            <Button variant="contained">Añadir ejercicio a la rutina</Button>
+          </div>
+        </AccordionDetails>
+      </Accordion>
     </>
   );
 };
